9b2b199245d0ea96bfeb1136451b1bba6cad9ee8
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-03-22 17:44:14 +0300

    fear: roning config complite

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,15 @@ import HomeWork_w1_1 from './stage_1/week_1/homework/homework-front-main/src/App
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        {/* Создаем ссылку */}
-        <Link to="/home-work-w1-1">Home work_1</Link>
-        
-        <Routes>
-          {/* Маршрут к другому проекту */}
-          <Route path="/home-work-w1-1" element={<HomeWork_w1_1 />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+    {/* Создаем ссылку */}
+    <Link to="/home-work-w1-1">Home work_1</Link>
+    
+    <Routes>
+      {/* Маршрут к другому проекту */}
+      <Route path="/home-work-w1-1" element={<HomeWork_w1_1 />} />
+    </Routes>
+  </div>
   );
 }
 
--- src/stage_1/week_1/homework/homework-front-main/src/App.tsx
+++ src/stage_1/week_1/homework/homework-front-main/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <h1>Hello</h1>
       {/*<HW1 />*/}
       {/*<HW2 />*/}
       {/*<HW3 />*/}
