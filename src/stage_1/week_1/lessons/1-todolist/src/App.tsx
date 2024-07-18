ab582efeecb3b0ee94138760c586534c41c78f2d
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-07-18 18:27:39 +0300

    feat: start axios project

--- src/stage_1/week_1/lessons/1-todolist/src/App.tsx
+++ src/stage_1/week_1/lessons/1-todolist/src/App.tsx
@@ -1,84 +1,130 @@
-import React, {useState} from 'react';
-import styles from "./components/Site.module.css";
-import { PageOne } from './components/pages/PageOne';
-import { PageTwo } from './components/pages/PageTwo';
-import { PageThree } from './components/pages/PageThree';
-import { Navigate, Route, Routes, NavLink, Link } from 'react-router-dom';
-import { Error404 } from './components/pages/Error404';
-import styled from 'styled-components'
+import React, { useCallback } from 'react'
+import './App.css';
+import { TaskType, Todolist } from './Todolist';
+import { AddItemForm } from './AddItemForm';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { Menu } from '@mui/icons-material';
+import {
+    addTodolistAC,
+    changeTodolistFilterAC,
+    changeTodolistTitleAC,
+    removeTodolistAC
+} from './state/todolists-reducer';
+import { addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC } from './state/tasks-reducer';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppRootStateType } from './state/store';
 
-const PATH = {
-    ADIDAS: '/adidas',
-    PUMA: '/puma',
-    ABIBAS: '/abibas',
-    ERROR: '/error404'
-} as const
+
+export type FilterValuesType = 'all' | 'active' | 'completed';
+export type TodolistType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
+
+export type TasksStateType = {
+    [key: string]: Array<TaskType>
+}
+
 
 function App() {
 
-    const getNavLinkClass = ({ isActive }: { isActive: boolean }) => {
-            return isActive ? styles.activeNavLink : styles.navLink;
-        };
-    
+    const todolists = useSelector<AppRootStateType, Array<TodolistType>>(state => state.todolists)
+    const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
+    const dispatch = useDispatch();
+
+    const removeTask = useCallback(function (id: string, todolistId: string) {
+        const action = removeTaskAC(id, todolistId);
+        dispatch(action);
+    }, []);
+
+    const addTask = useCallback(function (title: string, todolistId: string) {
+        const action = addTaskAC(title, todolistId);
+        dispatch(action);
+    }, []);
+
+    const changeStatus = useCallback(function (id: string, isDone: boolean, todolistId: string) {
+        const action = changeTaskStatusAC(id, isDone, todolistId);
+        dispatch(action);
+    }, []);
+
+    const changeTaskTitle = useCallback(function (id: string, newTitle: string, todolistId: string) {
+        const action = changeTaskTitleAC(id, newTitle, todolistId);
+        dispatch(action);
+    }, []);
+
+    const changeFilter = useCallback(function (value: FilterValuesType, todolistId: string) {
+        const action = changeTodolistFilterAC(todolistId, value);
+        dispatch(action);
+    }, []);
+
+    const removeTodolist = useCallback(function (id: string) {
+        const action = removeTodolistAC(id);
+        dispatch(action);
+    }, []);
+
+    const changeTodolistTitle = useCallback(function (id: string, title: string) {
+        const action = changeTodolistTitleAC(id, title);
+        dispatch(action);
+    }, []);
+
+    const addTodolist = useCallback((title: string) => {
+        const action = addTodolistAC(title);
+        dispatch(action);
+    }, [dispatch]);
+
     return (
-        <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                <nav className={styles.nav}>
-                    <ul>
-                        {/* <li><NavLink to ={'/adidas'} className={getNavLinkClass}>ADIDIAS</NavLink></li>
-                        <li> <NavLink to ={'/puma'} className={getNavLinkClass}>PUMA</NavLink> </li>
-                        <li> <NavLink to ={'/abibas'} className={getNavLinkClass}>ABIBAS</NavLink> </li> */}
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            <Container fixed>
+                <Grid container style={{padding: '20px'}}>
+                    <AddItemForm addItem={addTodolist}/>
+                </Grid>
+                <Grid container spacing={3}>
+                    {
+                        todolists.map(tl => {
+                            let allTodolistTasks = tasks[tl.id];
 
-                        <NavWrapper>
-                            <NavLink to ={PATH.ADIDAS} >ADIDIAS</NavLink>
-                        </NavWrapper>
-                        <NavWrapper>
-                            <NavLink to ={PATH.PUMA} >PUMA</NavLink>
-                        </NavWrapper>
-                        <NavWrapper>
-                            <NavLink to ={PATH.ABIBAS} >ABIBAS</NavLink>
-                        </NavWrapper>
-                    
-                    </ul>
-                </nav>
-                <div className={styles.content}>
-                    <Routes>
-                        <Route path='/' element={<Navigate to={'/adidas'}/>}/>
-
-                        <Route path={PATH.ADIDAS} element={<PageOne/>}/>
-                        <Route path={PATH.PUMA} element={<PageTwo/>}/>
-                        <Route path={PATH.ABIBAS} element={<PageThree/>}/>
-                        <Route path={PATH.ERROR} element={<Error404/>}/>
-
-                        <Route path='*' element={<Error404/>}/>
-                        
-                    </Routes>
-                </div>
-            </div>
-            <div className={styles.footer}>abibas 2023</div>
+                            return <Grid item key={tl.id}>
+                                <Paper style={{padding: '10px'}}>
+                                    <Todolist
+                                        id={tl.id}
+                                        title={tl.title}
+                                        tasks={allTodolistTasks}
+                                        removeTask={removeTask}
+                                        changeFilter={changeFilter}
+                                        addTask={addTask}
+                                        changeTaskStatus={changeStatus}
+                                        filter={tl.filter}
+                                        removeTodolist={removeTodolist}
+                                        changeTaskTitle={changeTaskTitle}
+                                        changeTodolistTitle={changeTodolistTitle}
+                                    />
+                                </Paper>
+                            </Grid>
+                        })
+                    }
+                </Grid>
+            </Container>
         </div>
     );
 }
-
-const NavWrapper = styled.li`
-    margin-left: 10px;
-    font-size: 20px;
-
-    & > a {
-        text-decoration: none;
-        color: #282c34;
-    }
-
-    & > a.active {
-        text-decoration: underline;
-        color: #7fb8f1;
-    }
-
-    & > a.hover {
-        color: #8cabc3;
-    }
-`
-
 
 export default App;
