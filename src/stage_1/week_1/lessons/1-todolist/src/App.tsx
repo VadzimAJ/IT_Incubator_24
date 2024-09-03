1530338cc9c724e1b17a6916036c8c952d26d6ca
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-09-03 03:18:49 +0300

    feat: repetition of the covered material from React Rout

--- src/stage_1/week_1/lessons/1-todolist/src/App.tsx
+++ src/stage_1/week_1/lessons/1-todolist/src/App.tsx
@@ -1,33 +1,84 @@
-import './App.css';
-import {Todolist} from './Todolist';
+import React, {useState} from 'react';
+import styles from "./components/Site.module.css";
+import { PageOne } from './components/pages/PageOne';
+import { PageTwo } from './components/pages/PageTwo';
+import { PageThree } from './components/pages/PageThree';
+import { Navigate, Route, Routes, NavLink, Link } from 'react-router-dom';
+import { Error404 } from './components/pages/Error404';
+import styled from 'styled-components'
 
-export type TaskType = {
-    id: number,
-    title: string,
-    isDone: boolean
-}
+const PATH = {
+    ADIDAS: '/adidas',
+    PUMA: '/puma',
+    ABIBAS: '/abibas',
+    ERROR: '/error404'
+} as const
 
 function App() {
 
-    const task1 = [
-        {id : 1, title: "HTML&CSS", isDone: false },
-        {id : 2, title: "JS -HUES&CSS", isDone: true },
-        {id : 3, title: "React", isDone: true},
-        {id : 4, title: "Pypurka", isDone: true}
-    ]
-
-    const task2 = [
-        {id : 1, title: "Potato", isDone: true },
-        {id : 2, title: "Bread", isDone: false },
-        {id : 3, title: "Oil", isDone: false }
-    ]
-
+    const getNavLinkClass = ({ isActive }: { isActive: boolean }) => {
+            return isActive ? styles.activeNavLink : styles.navLink;
+        };
+    
     return (
-        <div className="App">
-        <Todolist title="Whot to learn" task = {task1} />
-        <Todolist title="Whot to Buy" task = {task2} />
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <nav className={styles.nav}>
+                    <ul>
+                        {/* <li><NavLink to ={'/adidas'} className={getNavLinkClass}>ADIDIAS</NavLink></li>
+                        <li> <NavLink to ={'/puma'} className={getNavLinkClass}>PUMA</NavLink> </li>
+                        <li> <NavLink to ={'/abibas'} className={getNavLinkClass}>ABIBAS</NavLink> </li> */}
+
+                        <NavWrapper>
+                            <NavLink to ={PATH.ADIDAS} >ADIDIAS</NavLink>
+                        </NavWrapper>
+                        <NavWrapper>
+                            <NavLink to ={PATH.PUMA} >PUMA</NavLink>
+                        </NavWrapper>
+                        <NavWrapper>
+                            <NavLink to ={PATH.ABIBAS} >ABIBAS</NavLink>
+                        </NavWrapper>
+                    
+                    </ul>
+                </nav>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path='/' element={<Navigate to={'/adidas'}/>}/>
+
+                        <Route path={PATH.ADIDAS} element={<PageOne/>}/>
+                        <Route path={PATH.PUMA} element={<PageTwo/>}/>
+                        <Route path={PATH.ABIBAS} element={<PageThree/>}/>
+                        <Route path={PATH.ERROR} element={<Error404/>}/>
+
+                        <Route path='*' element={<Error404/>}/>
+                        
+                    </Routes>
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
         </div>
     );
 }
+
+const NavWrapper = styled.li`
+    margin-left: 10px;
+    font-size: 20px;
+
+    & > a {
+        text-decoration: none;
+        color: #282c34;
+    }
+
+    & > a.active {
+        text-decoration: underline;
+        color: #7fb8f1;
+    }
+
+    & > a.hover {
+        color: #8cabc3;
+    }
+`
+
 
 export default App;
